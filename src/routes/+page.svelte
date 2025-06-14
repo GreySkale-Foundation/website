71c25bfbc665d77fe1644e96228d844db6656e4a
Ashwin P Shine <ashwinshine.nonu@gmail.com>
2025-06-14 15:55:50 +0530

    fix(app): Gradient Base Landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<div class="min-h-screen w-full bg-linear-to-b from-black from-40% via-pink-400 via-50% to-white to-60%">
+<script>
+    import GSKLogo from '$lib/assets/gsklogo.svg';
+</script>
 
+
+<!-- class="min-h-48 w-full bg-linear-to-b from-black from-40% via-pink-400 via-50% to-white to-60%"> -->
+<div> 
+    <section class="min-h-[90vh] bg-black">
+
+    </section>
+    <section class="min-h-[30vh] bg-linear-to-b from-black from-30% via-pink-400 via-50% to-white to-70%">
+
+      <p><b>sadaadsadasdsadhakjsadda</b></p>
+
+    </section>
 </div>
