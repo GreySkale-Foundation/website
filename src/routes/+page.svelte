3f170a09ba74c355969f4a088b92e854399454f7
Chandra Rajesh <chandra5432141@gmail.com>
2025-06-14 22:53:08 +0530

    feat(app): Centered Logo and enabled hover effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,18 @@
 
 <!-- class="min-h-48 w-full bg-linear-to-b from-black from-40% via-pink-400 via-50% to-white to-60%"> -->
 <div> 
-    <section class="min-h-[90vh] bg-black">
+    <section class="flex items-center justify-center min-h-[90vh] bg-black">
+      <div class="group relative">
+    <img src={GSKLogo} alt="GreySkale"
+      class="w-64 h-auto transition-opacity duration-300 group-hover:opacity-0" />
 
+    <div
+    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center text-2xl font-semibold text-white"
+    style="font-family:'Trebuchet MS', 'sans-serif';">
+    Be who you ought to be.
+    </div>
+
+    </div>
     </section>
     <section class="min-h-[30vh] bg-linear-to-b from-black from-30% via-pink-400 via-50% to-white to-70%">
 
