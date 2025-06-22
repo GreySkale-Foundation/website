c6c81fefc02adda0c991de01d79845f28ec3d8f1
Ashwin <ashwinshine.nonu@gmail.com>
2025-06-22 23:15:53 +0530

    feat(app): Event Section Added

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Events from '$lib/components/events.svelte'
+    import EventData from '$lib/data/events.json'
+</script>
+
+<div class="w-[960px] ml-auto mr-auto">
+   <div class="grid auto-cols-[280px] auto-rows-[280px]  gap-4">
+  {#each EventData as image_data}
+     <div class={`col-span-${image_data.layout.col} ` + `row-span-${image_data.layout.row} rounded-2xl`}>
+        <Events image_data={image_data}/>
+     </div>
+  {/each}
+</div>
+</div>
