f2102754bfa24577c09618710663f13c640d5e9e
Chandra Rajesh <chandra5432141@gmail.com>
2025-06-27 19:11:27 +0530

    text-modified

---
+++ src/lib/components/tiles.svelte
@@ -0,0 +1,96 @@
+<script>
+</script>
+
+<div class="w-full py-16">
+  <div class="max-w-6xl mx-auto px-4">
+   
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mb-14">
+      
+      <div class="md:col-span-1 flex items-end">
+        <h1 class="text-3xl font-bold text-black">Check us out!</h1>
+      </div>
+
+      <div class="hidden md:block"></div>
+
+      
+      <div class="md:col-span-1 md:text-right flex items-end justify-end">
+        <p class="md-32 text-black">
+          We make content on YouTube, mostly tech related - tutorials,
+          podcasts and stories!
+        </p>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+      
+      <div
+        class="group h-[400px] bg-white rounded-2xl shadow-md overflow-hidden border border-gray-200 transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl"
+      >
+        <div class="p-7 h-full flex flex-col">
+          <div class="mb-6">
+            <h3
+              class="text-2xl font-bold text-black pb-2 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[50px] after:h-[3px] after:bg-black group-hover:after:w-full after:transition-all after:duration-300"
+            >
+              PODCASTS
+            </h3>
+          </div>
+          <div class="mt-auto">
+            <p class="text-black leading-relaxed">
+              We make podcasts every month featuring our members,
+              get ready to unwind genuine curious questions!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      
+      <div
+        class="group h-[400px] bg-white rounded-2xl shadow-md overflow-hidden border border-gray-200 transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl"
+      >
+        <div class="p-7 h-full flex flex-col">
+          <div class="mb-6">
+            <h3
+              class="text-2xl font-bold text-black pb-2 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[50px] after:h-[3px] after:bg-black group-hover:after:w-full after:transition-all after:duration-300"
+            >
+              TUTORIALS
+            </h3>
+          </div>
+          <div class="mt-auto">
+            <p class="text-black leading-relaxed">
+              Coding, design and much more!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      
+      <div
+        class="group h-[400px] bg-white rounded-2xl shadow-md overflow-hidden border border-gray-200 transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl"
+      >
+        <div class="p-7 h-full flex flex-col">
+          <div class="mb-6">
+            <h3
+              class="text-2xl font-bold text-black pb-2 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-[50px] after:h-[3px] after:bg-black group-hover:after:w-full after:transition-all after:duration-300"
+            >
+              TALK SESSIONS
+            </h3>
+          </div>
+          <div class="mt-auto">
+            <p class="text-black leading-relaxed">
+              Talk sessions also feature our members speaking
+              about a subject they are interested or skilled at.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="mt-12 text-center">
+      <p class="text-xl font-bold">
+        WATCH THESE IF YOU FIND THEM <span class="text-[#AE3579]">INTERESTING!!</span>
+      </p>
+    </div>
+  </div>
+</div>
+  
