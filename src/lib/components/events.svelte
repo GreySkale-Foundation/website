f4c3036152027302167c023060926d56f35196d4
Chandra Rajesh <chandra5432141@gmail.com>
2025-07-08 23:12:21 +0530

    improved bento

--- src/lib/components/events.svelte
+++ src/lib/components/events.svelte
@@ -3,12 +3,13 @@
     import Image from "$lib/assets/event/event.svg";
     import Image1 from "$lib/assets/event/Weekly.jpg";
     import Image2 from '$lib/assets/event/event2.svg'
+    import Major_event from "$lib/assets/event/Ppu.jpeg";
     import Newsletter from '$lib/assets/misc/june25.pdf';
     import {eventMaker} from "$lib/scripts/events"
     
-    const monthlyEvent = eventMaker("Monthly",Image2,"Hello How are you")
-    const majorEvent = eventMaker("MAIN EVENT", Image, "Very dope event")
-    const weeklyEvent = eventMaker("Weekly", Image1, "This is done by Chandra Rajesh")
+    const monthlyEvent = eventMaker("Monthly",Image2,"Our Montly Event")
+    const majorEvent = eventMaker("MAIN EVENT", Major_event, "Collaboration Event")
+    const weeklyEvent = eventMaker("Weekly", Image1, "Weekly Regular Event")
 </script>
 
 <div class="flex h-full w-full items-center justify-center px-2 sm:px-4">
@@ -51,11 +52,12 @@
 
         <!-- Bottom row: Blog cards -->
         <div class="grid grid-cols-2 gap-3">
-            <div class="relative h-[80px] rounded-lg shadow-md bg-pink-500 flex items-center justify-center">
-                <div class="text-white font-bold text-sm">Meet the Peeps</div>
+            <div class="relative h-[80px] shadow-md flex items-center justify-center bg-white border border-black font-courier-prime font-bold rounded-lg text-xl ">
+                <div class="text-black font-bold text-sm"> <a href="https://medium.com/@greyskalerobotics/behind-greyskale-79fbb6603944"> Read our Blog</a>
+                </div>
             </div>
-            <div class="relative h-[80px] rounded-lg shadow-md bg-pink-300 flex items-center justify-center">
-                <div class="text-gray-800 font-bold text-sm">Read our Blog</div>
+            <div class="relative h-[80px] shadow-md flex items-center justify-center bg-white border border-black font-courier-prime font-bold rounded-lg text-xl ">
+                <div class="text-black font-bold text-sm">Meet the Peeps</div>
             </div>
         </div>
     </div>
@@ -64,13 +66,13 @@
     <div class="hidden md:block w-full">
         <div class="grid h-full w-full gap-4 p-2 grid-cols-4 auto-rows-[175px] max-w-[1050px] mx-auto">
             <div class="relative col-span-2 row-span-1 rounded-lg shadow-md flex items-center justify-center">
-                <div class="h-full w-full flex justify-center items-center bg-pink-500 font-bold rounded-lg text-white">
-                    Blog
+                <div class="h-full w-full flex justify-center items-center bg-white border border-black font-courier-prime font-bold rounded-lg text-xl">
+                <a href="https://medium.com/@greyskalerobotics/behind-greyskale-79fbb6603944">Read our Blog</a>
                 </div>
             </div>
             <div class="relative col-span-2 row-span-1 rounded-lg shadow-md flex items-center justify-center">
-                <div class="h-full w-full flex justify-center items-center bg-pink-300 font-bold rounded-lg">
-                    Blog
+                <div class="h-full w-full flex justify-center items-center bg-white border border-black font-courier-prime font-bold rounded-lg text-xl">
+                Meet the Peeps
                 </div>
             </div>
             <div class="relative col-span-1 row-span-2 rounded-lg shadow-md flex items-center justify-center">
