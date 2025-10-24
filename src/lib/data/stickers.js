b739bdf092346cddc1d71aabb0a5cba34ae30765
Ashwin <ashwinshine.nonu@gmail.com>
2025-10-24 21:45:01 +0530

    refactor(app): Code changes and components made

---
+++ src/lib/data/stickers.js
@@ -0,0 +1,20 @@
+
+  import froggo from "$lib/assets/products/froggo.png";
+  import happyBuddy from "$lib/assets/products/happybuddy.png";
+  import milo from "$lib/assets/products/milo.png";
+  import lily from "$lib/assets/products/lily.png";
+  import sunny from "$lib/assets/products/sunny.png";
+  
+
+ 
+  // optional alt images (if you have them)
+ // import froggoAlt from "$lib/assets/products/froggo_alt.png";
+ // import happyBuddyAlt from "$lib/assets/products/happybuddy_alt.png";
+
+  export const stickers = [
+    { title: "Happy buddy", link : "happybuddy", desc: "A+ for effort!", img: happyBuddy, price: "15/-", altprice: "12/- only!" },
+    { title: "Froggoo", link : "frogo", desc: "I’m not afraid to be seen trying", img: froggo, price: "15/-" , altprice: "12/- only!"},
+    { title: "Flowers and Milo the cat",link : "flowersandmilo", desc: "Look around! Flowers Everywhere!", img: milo, price: "15/-",   altprice: "12/- only!" },
+    { title: "Lily and the musical box", link: "lilyandmusic", desc: "Bloom in your own pace", img: lily, price: "15/-", altprice: "12/- only!" },
+    { title: "Sunshine", link: "sunshine", desc: "Good things are coming", img: sunny, price: "15/-",  altprice: "12/- only!" }
+  ];
